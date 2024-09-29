3cff0d7086c4e3015c2f274749b1ed0f927d03b8
Dennis Njau <njaudennis30@gmail.com>
2024-09-29 14:53:47 +0300

    update projects.tsx

---
+++ src/components/sections/projects.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import BlurFade from "../blur-fade";
+import { BLUR_FADE_DELAY, DATA } from "@/lib/constants";
+import { ProjectCard } from "../project-card";
+
+export default function ProjectsSection() {
+  return (
+    <section id="projects">
+      <div className="w-full space-y-12 py-12">
+        <BlurFade delay={BLUR_FADE_DELAY * 11}>
+          <div className="flex flex-col items-center justify-center space-y-4 text-center">
+            <div className="space-y-2">
+              <div className="inline-block rounded-lg bg-foreground px-3 py-1 text-sm text-background">
+                My Projects
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Check out my <span className="text-primary">latest work</span>
+              </h2>
+              <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Here are a few of my favorite projects that I have worked on.
+              </p>
+            </div>
+          </div>
+        </BlurFade>
+        <div className="mx-auto grid max-w-[800px] grid-cols-1 gap-3 sm:grid-cols-2">
+          {DATA.projects.map((project, id) => (
+            <BlurFade
+              key={project.title}
+              delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+            >
+              <ProjectCard
+                href={project.href}
+                key={project.title}
+                title={project.title}
+                description={project.description}
+                tags={project.technologies}
+                image={project.image}
+                video={project.video}
+                links={project.links}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
