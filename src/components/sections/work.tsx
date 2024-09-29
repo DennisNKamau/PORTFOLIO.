92cae88f6806e000f659e817fb52a8872acb1331
Dennis Njau <njaudennis30@gmail.com>
2024-09-29 14:55:44 +0300

    update work.tsx

---
+++ src/components/sections/work.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import BlurFade from "../blur-fade";
+import { BLUR_FADE_DELAY, DATA } from "@/lib/constants";
+import { ResumeCard } from "../resume-card";
+
+export default function WorkSection() {
+  return (
+    <section id="work">
+      <div className="flex min-h-0 flex-col gap-y-3">
+        <BlurFade delay={BLUR_FADE_DELAY * 5}>
+          <h2 className="text-xl font-bold">Professional Experience</h2>
+        </BlurFade>
+
+        {DATA.work.map((work, id) => (
+          <BlurFade key={work.company} delay={BLUR_FADE_DELAY * 6 + id * 0.05}>
+            <ResumeCard
+              key={work.company}
+              logoUrl={work.logoUrl}
+              altText={work.company}
+              title={work.company}
+              subtitle={work.title}
+              href={work.href}
+              badges={work.badges}
+              period={`${work.start} - ${work.end ?? "Present"}`}
+              description={work.description}
+            />
+          </BlurFade>
+        ))}
+      </div>
+    </section>
+  );
+}
